ec6195266c983c73c7e6613ac096eca3db5cd130
apase95 <noobyhandsome@gmail.com>
2026-01-14 21:21:34 +0700

    Setup Environment

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+      }
+    }
+  }
+})
